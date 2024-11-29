f6b62daa3429f7021102c3353ca0d2f9cd1e7c03
soliyana wasiyhun <soliyana.wasiyhun@aastustudent.edu.et>
2024-11-29 01:08:45 -0800

    Update soii.cpp

--- ETS1304_Soliyana_Wasiyhun/soii.cpp
+++ ETS1304_Soliyana_Wasiyhun/soii.cpp
@@ -1,3 +1,6 @@
+/*take a program that calculates the Body Mass Index (BMI) of a person and determine weather the person is
+in normal weight, under weight or over weight. BMI is calculated as (BMI = w/h*h). Make the program to
+work for multiple person before terminated.*/
 #include <iostream>
 
 using namespace std;
