06f53229aaddaa79871de7ff405d29d166747f4a
sosnage12 <sosna.gemechu@aastustudent.edu.et>
2024-11-29 11:10:56 +0300

    Update Numbering.cpp

--- ETS1313 _sosna_gemechu/Numbering.cpp
+++ ETS1313 _sosna_gemechu/Numbering.cpp
@@ -1,1 +1,30 @@
-
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+float height, weight, BMI;
+block: 
+cout<<"please enter your weight "<<endl;
+cin>>weight;
+cout<<"please enter your height "<<endl;
+cin>>height;
+BMI=weight/(height*height);
+if(BMI<18.5){
+    cout<<"oops! you are under weight. your body mass index is  "<<BMI<<endl;
+} else if(BMI>18.5&&BMI<24.9){
+    cout<<"great you are at normal weight. your body mass index is  "<<BMI<<endl;
+} else if(BMI>24.9&&BMI<29.9){
+    cout<<"great you are at over weight. your body mass index is  "<<BMI<<endl;
+} else {
+    cout<<"oops! you are obese you need to work exrcise every day. your body mass index is  "<<BMI;
+}
+int number;
+cout<<"if you want to continue enter /' 1/' and if you don/'t enter /'0/'"<<endl;
+cin>>number;
+if(number==1){
+goto block;
+} else if(number==0){
+cout<<"we finish thank you"<<endl;
+}
+    return 0;
+}
