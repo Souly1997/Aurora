805abba4a034bc0d6031bef977510db316e28d04
Semehal Hailu Woldeslassie <semehal.hailu@aastustudent.edu.et>
2024-11-29 14:55:29 +0300

    Create Assignment_1_question_3.cpp

---
+++ ETS1266_Semehal_Hailu/Assignment_1_question_3.cpp
@@ -0,0 +1,18 @@
+/*Write a program that find the result of the expression 𝑥^𝑦 where the value of X and Y are entered by the user.*/
+
+#include<iostream>
+using namespace std;
+int main (){
+    int x,y,product=1;
+    cout<<"enter x : "<<endl;
+    cin>>x;
+    cout<<"enter y : "<<endl;
+    cin>>y;
+    for(int i=1;i<=y;i++){
+         product*=x;
+    }
+    cout<<x<<" "<<"the power of"<<y<<" "<<" =  "<<product<<endl;
+
+
+    return  0;
+}
