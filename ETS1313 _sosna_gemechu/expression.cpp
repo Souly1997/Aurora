b25b31d8e24ee986231052e82309c63664c5eb95
sosnage12 <sosna.gemechu@aastustudent.edu.et>
2024-11-30 10:05:22 +0300

    Update expression.cpp

--- ETS1313 _sosna_gemechu/expression.cpp
+++ ETS1313 _sosna_gemechu/expression.cpp
@@ -2,11 +2,13 @@
 #include <iostream>
 using namespace std;
 int main(){
-int X, Y ,product=1;
-cout<<"enter two integer number "<<endl;
+int X, Y ,product=1; /*Starting with 1  for product is consistent with the concept of calculating the cumulative product, 
+    where we begin with an identity value (1) and multiply successively by other numbers*/
+cout<<"enter two integer number "<<endl; //first we accept two integer from the user 
 cin>>X;
-cout<<"and ";
+cout<<"and enter the other ";
 cin>>Y;
+    //we use for loop because we know how many times the X multiple by it self itrate(Y times) 
 for(int i=1;i<=Y;i++){
     product*=X;
     cout<<X<<" the power of "<<Y<<" is"<<product<<endl;
