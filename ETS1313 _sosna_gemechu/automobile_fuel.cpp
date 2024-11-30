63c816f14b064bcc89f551d715f850d12a3da013
sosnage12 <sosna.gemechu@aastustudent.edu.et>
2024-11-30 09:29:02 +0300

    Create automobile_fuel.cpp

---
+++ ETS1313 _sosna_gemechu/automobile_fuel.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+float gallon, gallon_capacity, mile;
+cout<<"enter how much gallon use your automobie  to  drive 1 mile   "<<endl;
+cin>>gallon_capacity;cout<<"gallon your mobile use to drive one mile "<<endl;
+cout<<"How much gallon do you have in your automobile fuel tank and please enter in gallon"<<endl;
+cin>>gallon;cout<<"gallon"<<endl;
+mile=gallon_capacity/gallon;
+cout<<"Your automobile can drive "<<mile<<"mile with out refueling Thank you!! be safe!";
+
+    return 0;
+}
