fd0c079e8b264104ddb04569bcc2250128fced8c
Semehal Hailu Woldeslassie <semehal.hailu@aastustudent.edu.et>
2024-12-02 00:04:35 +0300

    Update Assignment_1_Question_1.cpp

--- ETS1266_Semehal_Hailu/Assignment_1_Question_1.cpp
+++ ETS1266_Semehal_Hailu/Assignment_1_Question_1.cpp
@@ -1,45 +1,35 @@
-/*Make a program that calculates the Body Mass Index (BMI) of a person and determine weather the person is
+Make a program that calculates the Body Mass Index (BMI) of a person and determine weather the person is
 in normal weight, under weight or over weight. BMI is calculated as (BMI = w/h*h). Make the program to
-work for multiple person before terminated.*/
+work for multiple person before terminated ?
 
 
-#include<iostream>
+#include <iostream>
+#include <cmath>
 using namespace std;
 int main(){
-    float weight,height,BMI,choice;
-    do
-    {
-     cout<<"Please enter your weight : "<<endl;
-    cin>>weight;
-    cout<<"Please enter your height : "<<endl;
-    cin>>height;
-    BMI=weight/ (height* height);
-    cout<<"BMI = "<<BMI<<endl;
-      if (BMI<18.5)
-      {
-        cout<<"underweight"<<endl;
-    }else if (BMI>18.5 && BMI<24.9)
-    {
-        cout<<"normal weight"<<endl;
-    }else if (BMI<25 && BMI >29.9)
-    {
-        cout<<"overweight"<<endl;
-    }else{
-        cout<<"obese"<<endl;
-    } 
-    cout<<"Do you want to continue if so press 1 if not press 0"<<endl;
-    cin>>choice;
-    }
-    while (choice=='0');
-    cout<<"we finish thank you";
-    
-    
-
-    
-    
-    
-    
-    
+float height, weight, BMI;
+block: 
+cout<<"please enter your weight "<<endl;
+cin>>weight;
+cout<<"please enter your height "<<endl;
+cin>>height;
+BMI=weight/(height*height);
+if(BMI<18.5){
+    cout<<"oops! you are under weight. your body mass index is  "<<BMI<<endl;
+} else if(BMI>18.5&&BMI<24.9){
+    cout<<"great you are at normal weight. your body mass index is  "<<BMI<<endl;
+} else if(BMI>24.9&&BMI<29.9){
+    cout<<"great you are at over weight. your body mass index is  "<<BMI<<endl;
+} else {
+    cout<<"oops! you are obese you need to work exrcise every day. your body mass index is  "<<BMI;
+}
+int number;
+cout<<"if you want to continue enter /' 1/' and if you don/'t enter /'0/'"<<endl;
+cin>>number;
+if(number==1){
+goto block;
+} else if(number==0){
+cout<<"we finish thank you"<<endl;
+}
     return 0;
 }
-
